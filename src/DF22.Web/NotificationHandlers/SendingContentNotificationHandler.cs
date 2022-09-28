d6682444516f8964f123e94eb3782c8c94640a4e
Dave Woestenborghs <dave.woestenborghs@dotcontrol.com>
2022-09-28 17:27:55 +0200

    Hide property for non admins

--- src/DF22.Web/NotificationHandlers/SendingContentNotificationHandler.cs
+++ src/DF22.Web/NotificationHandlers/SendingContentNotificationHandler.cs
@@ -1,14 +1,50 @@
 ﻿using Umbraco.Cms.Core.Events;
+using Umbraco.Cms.Core.Models.ContentEditing;
 using Umbraco.Cms.Core.Notifications;
+using Umbraco.Cms.Core.Security;
 using Umbraco.Cms.Web.Common.PublishedModels;
 
 namespace DF22.Web.NotificationHandlers
 {
     internal sealed class SendingContentNotificationHandler : INotificationHandler<SendingContentNotification>
     {
+        private readonly IBackOfficeSecurityAccessor _backOfficeSecurityAccessor;
+
+        public SendingContentNotificationHandler(IBackOfficeSecurityAccessor backOfficeSecurityAccessor)
+        {
+            _backOfficeSecurityAccessor = backOfficeSecurityAccessor;
+        }
+
         public void Handle(SendingContentNotification notification)
         {
             this.PreventUnPublishing(notification);
+
+            var user = this._backOfficeSecurityAccessor.BackOfficeSecurity?.CurrentUser;
+
+            if (user != null && user.Groups.Any(x => x.Alias == Umbraco.Cms.Core.Constants.Security.AdminGroupAlias) ==
+                false)
+            {
+                // hide properties for non admins
+                this.HideMaintenanceProperty(notification);
+            }
+
+           
+        }
+
+        private void HideMaintenanceProperty(SendingContentNotification notification)
+        {
+            if (notification.Content.ContentTypeAlias.Equals(Home.ModelTypeAlias))
+            {
+                foreach (var variant in notification.Content.Variants)
+                {
+                    foreach (var tab in variant.Tabs)
+                    {
+                        tab.Properties =
+                            (tab.Properties ?? Array.Empty<ContentPropertyDisplay>()).Where(x =>
+                                x.Alias != "isUnderMaintenance");
+                    }
+                }
+            }
         }
 
         private void PreventUnPublishing(SendingContentNotification notification)
