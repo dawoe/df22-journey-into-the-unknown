4fd16c5996034f8473b3e31ebeb2c58d255f54f2
Dave Woestenborghs <dave.woestenborghs@dotcontrol.com>
2022-10-06 16:40:10 +0200

    Added custom route for sitemap xml

--- src/DF22.Web/DF22Composer.cs
+++ src/DF22.Web/DF22Composer.cs
@@ -3,9 +3,12 @@ using DF22.Web.NotificationHandlers;
 using DF22.Web.Routing.ContentFinders;
 using DF22.Web.Routing.UrlProviders;
 using DF22.Web.Routing.UrlSegmentProviders;
+using DF22.Web.Routing.VirtualRoutes;
 using Umbraco.Cms.Core.Composing;
 using Umbraco.Cms.Core.Notifications;
 using Umbraco.Cms.Infrastructure.Examine;
+using Umbraco.Cms.Web.Common.ApplicationBuilder;
+using Umbraco.Cms.Web.Common.Routing;
 
 namespace DF22.Web
 {
@@ -24,10 +27,40 @@ namespace DF22.Web
             // register custom search fields
             builder.Services.AddUnique<IUmbracoTreeSearcherFields, CustomSearchFields>();
 
-            // register routing stuff
+            // register routing extensions
             builder.UrlSegmentProviders().Insert<ProductUrlSegmentProvider>();
             builder.UrlProviders().Insert<BlogUrlProvider>();
             builder.ContentFinders().Append<BlogContentFinder>();
+
+            //handle sitemap.xml as server side request
+            builder.Services.Configure<UmbracoRequestOptions>(options =>
+            {
+                var allowList = new[] { "/sitemap.xml" };
+                options.HandleAsServerSideRequest = httpRequest =>
+                {
+                    foreach (var route in allowList)
+                    {
+                        if (httpRequest.Path.StartsWithSegments(route))
+                        {
+                            return true;
+                        }
+                    }
+
+                    return false;
+                };
+            });
+
+            // register virtual route for server side request
+            builder.Services.Configure<UmbracoPipelineOptions>(options =>
+            {
+                options.AddFilter(new UmbracoPipelineFilter(nameof(SitemapController))
+                {
+                    Endpoints = app => app.UseEndpoints(endpoints => endpoints.MapControllerRoute(
+                        "Xml SiteMap Controller",
+                        "/sitemap.xml",
+                        new { Controller = "Sitemap", Action = "Index" })),
+                });
+            });
         }
     }
 }
---
+++ src/DF22.Web/Routing/VirtualRoutes/SitemapController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using System.Xml.Linq;
+using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Web.Common.Controllers;
+using Umbraco.Cms.Web.Common.PublishedModels;
+
+namespace DF22.Web.Routing.VirtualRoutes
+{
+    public class SitemapController : UmbracoPageController, IVirtualPageController
+    {
+        private readonly XNamespace xmlNameSpace = "http://www.sitemaps.org/schemas/sitemap/0.9";
+
+
+        public SitemapController(ILogger<UmbracoPageController> logger, ICompositeViewEngine compositeViewEngine, IUmbracoContextAccessor umbracoContextAccessor) : base(logger, compositeViewEngine)
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return this.Content(this.GetSiteMapContents(), "text/xml");
+        }
+
+        public IPublishedContent? FindContent(ActionExecutingContext actionExecutingContext)
+        {
+            var umbracoContextAccessor = actionExecutingContext.HttpContext.RequestServices
+                .GetRequiredService<IUmbracoContextAccessor>();
+
+            if (umbracoContextAccessor.TryGetUmbracoContext(out var umbracoContext) == false || umbracoContext?.Content == null)
+            {
+                return null;
+            }
+
+            return umbracoContext.Content.GetAtRoot().OfType<Home>().FirstOrDefault();
+        }
+
+        private List<(string Url, DateTime LastModified)> GetSiteMapItems()
+        {
+            var pagesForSiteMap =
+                this.CurrentPage?.DescendantsOrSelf().Where(x => x.IsDocumentType(MaintenancePage.ModelTypeAlias) == false)
+                    .ToList() ?? new List<IPublishedContent>();
+
+            var siteMapItems = new List<(string Url, DateTime LastModified)>();
+
+            foreach (var page in pagesForSiteMap)
+            {
+                siteMapItems.Add(new ValueTuple<string, DateTime>(page.Url(mode: UrlMode.Absolute), page.UpdateDate));
+            }
+
+            return siteMapItems;
+        }
+
+        private string GetSiteMapContents()
+        {
+            var siteMapItems = GetSiteMapItems();
+
+            var sitemap = new XDocument(
+                new XDeclaration("1.0", "utf-8", "yes"),
+                new XElement(
+                    this.xmlNameSpace + "urlset",
+                    this.GetSiteMapItems().Select(this.CreateItemElement)));
+
+            return sitemap.ToString();
+        }
+
+        private XElement CreateItemElement((string Url, DateTime LastModified) item)
+        {
+            var itemElement = new XElement(this.xmlNameSpace + "url");
+
+            itemElement.Add(new XElement(this.xmlNameSpace + "loc", item.Url));
+            itemElement.Add(new XElement(this.xmlNameSpace + "lastmod", item.LastModified.ToString("yyyy-MM-ddTHH:mm:ss.f") + "+00:00"));
+
+            return itemElement;
+        }
+    }
+}
